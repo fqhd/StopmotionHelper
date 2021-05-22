2da6a0cab3535a9c48eb5b17e1ffee9d13806ff3
Fahd <fahd31256@gmail.com>
2021-05-22 13:57:25 +0100

    download button

--- src/main.js
+++ src/main.js
@@ -7,3 +7,21 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
 		video.srcObject = stream;
 	});
 }
+
+document.getElementById("snap").addEventListener("click", () => {
+	context.drawImage(video, 0, 0, 640, 480);
+	downloadImage(canvas.toDataURL());
+})
+
+async function downloadImage(imageSrc) {
+	const image = await fetch(imageSrc)
+	const imageBlog = await image.blob()
+	const imageURL = URL.createObjectURL(imageBlog)
+
+	const link = document.createElement('a')
+	link.href = imageURL
+	link.download = 'image file name here'
+	document.body.appendChild(link)
+	link.click()
+	document.body.removeChild(link)
+}
